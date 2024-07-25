5d4c5b6399c3214add85ded7037fb03aed2a2e94
Fikri Bachtiar <fikri.a.bachtiar@polytron.co.id>
2024-07-25 21:20:38 +0700

    chore(api): response change using scheme

--- app/routers/api_books.py
+++ app/routers/api_books.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Body, Response, status, HTTPException
-from app.helpers.response import create_response
 from app.helpers.enums import ResponseError
+from app.schemas.sche_base import ResponseSchemaBase
 
 router = APIRouter(
     prefix="/books",
@@ -10,18 +10,18 @@ router = APIRouter(
 # #for the purpose of this illustration, a list instead of a model will be used
 fake_books_db = []
 
-@router.post("/create_book", status_code=status.HTTP_201_CREATED)
+@router.post("/create_book", status_code=status.HTTP_201_CREATED, response_model=ResponseSchemaBase)
 async def create_new_book(response: Response, new_book=Body()):
 	for book in fake_books_db:
 		if book['title'] == new_book['title']:
-			return create_response(error=ResponseError.ERROR, message="book with this title already exists")
+			return ResponseSchemaBase().response(error=ResponseError.ERROR, message="book with this title already exists")
 	
 	fake_books_db.append(new_book)
-	return create_response(error=ResponseError.NO_ERROR, message="book added successfully")
+	return ResponseSchemaBase().response(error=ResponseError.NO_ERROR, message="book added successfully")
 
 @router.get('')
 async def read_all_books(response: Response):
-	return create_response(error=ResponseError.NO_ERROR, message=fake_books_db)
+	return ResponseSchemaBase().response(error=ResponseError.NO_ERROR, message=fake_books_db)
 
 @router.get('/')
 async def read_all_books_with_category(response: Response, category: str):
@@ -29,21 +29,21 @@ async def read_all_books_with_category(response: Response, category: str):
 	for book in fake_books_db:
 		if book.get('category').casefold() == category.casefold():
 			book_category.append(book)
-			return create_response(error=ResponseError.NO_ERROR, message=book_category)
-	return create_response(error=ResponseError.ERROR, message="no book with that category available")
+			return ResponseSchemaBase().response(error=ResponseError.NO_ERROR, message=book_category)
+	return ResponseSchemaBase().response(error=ResponseError.ERROR, message="no book with that category available")
 
 @router.put('/update_book')
 async def update_book(response: Response, update_book=Body()):
 	for i in range(len(fake_books_db)):
 		if fake_books_db[i].get('title').casefold() == update_book.get('title').casefold():
 			fake_books_db[i] = update_book
-			return create_response(error=ResponseError.NO_ERROR, message="book updated successfully")
-	return create_response(error=ResponseError.ERROR, message="no book with that title available")
+			return ResponseSchemaBase().response(error=ResponseError.NO_ERROR, message="book updated successfully")
+	return ResponseSchemaBase().response(error=ResponseError.ERROR, message="no book with that title available")
 
 @router.delete('/delete_book')
 async def delete_book(response: Response, book_title: str):
 	for i in range(len(fake_books_db)):
 		if fake_books_db[i].get('title').casefold() == book_title.casefold():
 			fake_books_db.pop(i)
-			return create_response(error=ResponseError.NO_ERROR, message="book deleted successfully")
-	return create_response(error=ResponseError.ERROR, message="no book with that title available")
+			return ResponseSchemaBase().response(error=ResponseError.NO_ERROR, message="book deleted successfully")
+	return ResponseSchemaBase().response(error=ResponseError.ERROR, message="no book with that title available")
---
+++ app/schemas/sche_base.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Any
+from app.helpers.enums import ResponseError
+
+class ResponseSchemaBase(BaseModel):
+    __abstract__ = True
+
+    error: ResponseError = ResponseError.NO_ERROR
+    message: Any = ""
+
+    def response(self, error: ResponseError, message: str):
+        self.error = error
+        self.message = message
+        return self
+
+    def success_response(self):
+        self.error = 0
+        self.message = 'success'
+        return self
+    
+    def failed_response(self):
+        self.error = 0
+        self.message = 'failed'
+        return self
