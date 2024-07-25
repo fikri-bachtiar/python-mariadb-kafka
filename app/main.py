c4c83d455808bb82aea9da2a4edfd19604fd5c38
Fikri Bachtiar <fikri.a.bachtiar@polytron.co.id>
2024-07-25 16:27:31 +0700

    build(main): general response code for no api found

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 import uvicorn
-from fastapi import Depends, FastAPI
-from .config import settings
-from .routers import books
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from app.config import settings
+from app.routers import books
 
 app = FastAPI(title=settings.app_project_name,
               description='Base frame with FastAPI micro framework + MariaDB + Kafka',
               version=settings.app_project_version)
 
 app.include_router(books.router)
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return JSONResponse(
+            status_code=404,
+            content={"error": 1, "message": "api endpoint does not exist"},
+        )
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": 1, "message":{"detail": exc.detail}},
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app, host=settings.app_running_host, port=settings.app_running_port)
