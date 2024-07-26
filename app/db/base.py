9077b23db75738235482d0cd8673724e16bd68e9
Fikri Bachtiar <fikri.a.bachtiar@polytron.co.id>
2024-07-26 11:32:05 +0700

    build(db): update import

--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
@@ -12,6 +12,8 @@ SQLALCHEMY_DATABASE_URL = "mariadb+mariadbconnector://{}:{}@{}:{}/{}".format(
     settings.app_maria_db_name,
 )
 
+# SQLALCHEMY_DATABASE_URL = "mariadb+mariadbconnector://example_user:example_password@127.0.0.1:3306/example_database"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     # connect_args={"check_same_thread": False}
@@ -19,7 +21,6 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 # Dependency
 def get_db():
